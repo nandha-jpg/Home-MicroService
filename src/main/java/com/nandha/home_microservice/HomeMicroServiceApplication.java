e61c53cd7d47190ee8f842b590ee0f8f80476938
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-16 16:09:05 +0530

    Initial commit

---
+++ src/main/java/com/nandha/home_microservice/HomeMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nandha.home_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMicroServiceApplication.class, args);
+	}
+
+}
