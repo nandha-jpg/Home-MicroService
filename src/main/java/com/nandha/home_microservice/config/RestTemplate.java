3dfb1bdbb735b7c35e082d5272cd9a8f9570503b
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-16 16:24:46 +0530

    created a config class which will return Rest template object.

---
+++ src/main/java/com/nandha/home_microservice/config/RestTemplate.java
@@ -0,0 +1,13 @@
+package com.nandha.home_microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplate {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
